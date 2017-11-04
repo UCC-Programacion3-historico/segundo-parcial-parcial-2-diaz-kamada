8da370efb3f2c1689fdbe9e2f7021bceea377f34
Cristian78 <kamadacristian@gmail.com>
2017-11-04 00:16:08 -0300

    modelo main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
 #include "MailManager.h"
 
-int main() {
-    std::cout << "Super gestor de mails" << std::endl;
+using namespace std;
+
+int main(){
+    int variable;
+    do{
+
+        cout << endl
+             << " 1 - Agregar Mail.\n"
+             << " 2 - Borrar Mail\n"
+             << " 3 - Buscar Mail\n"
+             << " 4 - Mostrar Mails\n"
+             << " 5 - Salir\n"
+             << " Ingrese su opcion y presione enter: ";
+        cin >> variable;
+
+        switch(variable)
+        {
+            case 1:
+                cout<<"Agregar"<<endl;
+
+                break;
+
+            case 2:
+                cout<<"ingrese id del correo a borrar"<<endl;
+
+                break;
+
+            case 3:
+                cout<<"ingrese remitente o palabra clave"<<endl;
+
+                break;
+
+            case 4:
+                cout<<"Mostrar"<<endl;
+
+                break;
+
+            case 5:
+                cout<<"Salir"<<endl;
+                break;
+
+        }
+    }while(variable!=5);
 
     return 0;
 }
