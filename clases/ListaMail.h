19902c5c6327a258f7332c57e81292fc161b8e20
Cristian78 <kamadacristian@gmail.com>
2017-11-04 00:54:56 -0300

    Implementacion de algunas funciones listas

--- clases/ListaMail.h
+++ clases/ListaMail.h
@@ -25,7 +25,7 @@ public:
 
     void remover(int pos);
 
-    email getDato(int pos);
+    email getMail(int pos);
 
     void reemplazar(int pos, email mail);
 
@@ -57,6 +57,48 @@ ListaMail::~ListaMail() {
 
 bool ListaMail::esVacia() {
     return inicio == NULL;
+}
+
+email ListaMail::getMail(int pos) {
+    int i = 0;
+    NodoMail *aux = inicio;
+    while (i < pos && aux != NULL) {
+        aux = aux->getNext();
+        i++;
+    }
+
+    if (aux == NULL)
+        throw 1;
+
+    return aux->getMail();
+}
+
+
+void ListaMail::reemplazar(int pos, email mail) {
+    int i = 0;
+    NodoMail *aux = inicio;
+    while (i < pos && aux != NULL) {
+        aux = aux->getNext();
+        i++;
+    }
+
+    if (aux == NULL)
+        throw 1;
+
+    aux->setMail(mail);
+}
+
+
+void ListaMail::vaciar() {
+    NodoMail *borr;
+    NodoMail *aux = inicio;
+
+    while (aux != NULL) {
+        borr = aux;
+        aux = aux->getNext();
+        delete[] borr;
+    }
+    inicio = NULL;
 }
 
 #endif //MAILMANAGER_LISTAMAIL_H
